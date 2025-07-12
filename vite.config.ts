e34be2c6300266cd4617371dbe312a9dc81169ca
serloro <32571944+serloro@users.noreply.github.com>
2025-07-12 08:26:46 +0200

    fix: corregir base URL para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/webUniConvert/',
+  base: '/webUniConverter/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
